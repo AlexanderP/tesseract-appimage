1958fe9944c60a34c77209e18e0be7fbbce950fa
Alexander Pozdnyakov <almipo@mail.ru>
2026-08-30 09:36:56 +0300

    test.sh update
    (cherry picked from commit a8af0db60525f8e6a529d59a0a4677d800aaa127)

--- test.sh
+++ test.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-DIR=$(pwd)
-DOCKER_DIR=${DIR}/test
-green() { echo -e "\\e[1;32m$1\\e[0;39;49m"; }
-red() { echo -e "\\e[1;31m$1\\e[0;39;49m"; }
-blue() { echo -e "\\e[1;34m$1\\e[0;39;49m"; }
-green_n() { echo -en "\\e[1;32m$1\\e[0;39;49m"; }
+DOCKER_DIR="$(pwd)/test"
+if [ ! -d "${DOCKER_DIR}" ]; then
+    red "Директория ${DOCKER_DIR} не существует"
+    exit 1
+fi
+
+if [ -t 1 ]; then
+    green() { echo -e "\\e[1;32m$1\\e[0;39;49m"; }
+    red() { echo -e "\\e[1;31m$1\\e[0;39;49m"; }
+    blue() { echo -e "\\e[1;34m$1\\e[0;39;49m"; }
+    green_n() { echo -en "\\e[1;32m$1\\e[0;39;49m"; }
+else
+    red() { echo "$1"; }
+    blue() { echo "$1"; }
+    green() { echo "$1"; }
+    green_n() { echo -n "$1"; }
+fi
+
+
 DIST=("almalinux_8" \
       "almalinux_9" \
       "almalinux_10" \
@@ -14,10 +27,6 @@ DIST=("almalinux_8" \
       "debian_11" \
       "debian_12" \
       "debian_13" \
-      "fedora_38" \
-      "fedora_39" \
-      "fedora_40" \
-      "fedora_41" \
       "fedora_42" \
       "ubuntu_14.04" \
       "ubuntu_16.04" \
@@ -25,34 +34,36 @@ DIST=("almalinux_8" \
       "ubuntu_20.04" \
       "ubuntu_22.04" \
       "ubuntu_24.04" \
-      "ubuntu_25.04" \
-      "opensuse_15.3" \
-      "opensuse_15.4" \
-      "opensuse_15.5"\
-      "opensuse_15.6"\
-      "opensuse_tumbleweed"\
+      "opensuse_15.6" \
+      "opensuse_tumbleweed" \
       "archlinux" \
       "fedora_43" \
+      "fedora_44" \
+      "ubuntu_26.04" \
       "opensuse_16.0" \
     )
-PICTURE="Apache.gif Apache.jpg Apache.png Apache.tif Apache.webp"
+PICTURES=("Apache.gif" "Apache.jpg" "Apache.png" "Apache.tif" "Apache.webp")
+
+if ! command -v docker &>/dev/null; then
+    red "Docker не установлен"
+    exit 1
+fi
 
 create_images(){
     for _images in "${DIST[@]}" ; do
-        if docker images | grep -q "tess_${_images}"
+        if docker image inspect "tess_${_images}" &>/dev/null
         then
             green "Образ ${_images} создан"
         else
             green "Создание образа ${_images}"
-            cd "${DOCKER_DIR}/${_images}"  || exit 1
-            docker build -t "tess_${_images}" .  || exit 1
+            docker build -t "tess_${_images}" "${DOCKER_DIR}/${_images}" || { red "Ошибка сборки образа ${_images}"; exit 1; }
         fi
     done
 }
 
 rm_images(){
     for _images in "${DIST[@]}" ; do
-        if docker images | grep -q "${_images}"
+        if docker image inspect "tess_${_images}" &>/dev/null
         then
             green "Удаление образа ${_images}"
             docker image rm "tess_${_images}" || exit 1
@@ -60,11 +71,11 @@ rm_images(){
     done
 }
 
-test_teseeract(){
+test_tesseract(){
     docker_run(){
-        if [ "${TRUE}" -eq 1 ]
+        if [ "${FAIL_TRUE}" -eq 1 ]
         then
-            docker run --rm -v "$PWD:/build" \
+            timeout 300 docker run --rm -v "$PWD:/build" \
                            --device=/dev/fuse \
                            --cap-add SYS_ADMIN \
                            --security-opt apparmor:unconfined \
@@ -73,7 +84,7 @@ test_teseeract(){
                 green "[OK]"
             else
                 red "[Ошибка]"
-                TRUE=0
+                FAIL_TRUE=0
             fi
         else
             blue "[Пропуск]"
@@ -83,30 +94,33 @@ test_teseeract(){
     for _images in "${DIST[@]}" ; do
         green   "               ${_images}              "
         green   "_______________________________________"
-        TRUE=1
+        FAIL_TRUE=1
         log_file=/tmp/log_${_images}
-        test ! -f "${log_file}" || rm "${log_file}" && touch "${log_file}"
-        if docker images | grep -q "${_images}"
+        > "${log_file}"
+        if docker image inspect "tess_${_images}" &>/dev/null
         then
-            echo -e "Проверка версии\n" >> "${log_file}"
+            echo -e "Проверка версии" >> "${log_file}"
             green_n "Проверка версии                    "
             docker_run "tess_${_images}" "${1}" -v
             green_n "Проверка списка языков             "
-            echo -e "\nПроверка списка языков\n" >> "${log_file}"
+            echo -e "Проверка списка языков" >> "${log_file}"
             docker_run "tess_${_images}" "${1}" --list-langs
-            for _picture in ${PICTURE}; do
-                echo -e "\nПроверка распознавания ${_picture}\n" >> "${log_file}"
+            for _picture in "${PICTURES[@]}"; do
+                echo -e "Проверка распознавания ${_picture}" >> "${log_file}"
                 green_n "Проверка распознавания ${_picture}  "
                 docker_run "tess_${_images}" "${1}" ${_picture} -
             done
         else
             red "Образ ${_images} несуществует "
         fi
-        unset TRUE
+        unset FAIL_TRUE
         echo ""
     done
 }
 
-#create_images
-#rm_images
-test_teseeract "$1"
+case "${1}" in
+    create) create_images ;;
+    remove) rm_images ;;
+    test)   test_tesseract "${2:-tesseract}" ;;
+    *)      echo "Usage: $0 {create|remove|test} [tesseract.appimage]"; exit 1 ;;
+esac
